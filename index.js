9d191ea7f056113e96167cc6e45fc23f1c952eca
awfljaeruoiqwr <114899835+awfljaeruoiqwr@users.noreply.github.com>
2024-01-30 10:44:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 
 const socket = require('socket.io');
+// 서버 시작
 const server = app.listen(3000, () => {
     console.log('Server is running');
 });
 
+// HTTP요청의 body를 파싱하기 위한 모듈 선언
 const bodyParser = require('body-parser');
 const SerialPort = require('./serialport/serialport');
 const { initializeSerialPort, robotSpeedToRPMSpeed, putPNTVelCmd, goal_rpm_speed } = require('./serialport/serialport');
+// JSON 형태의 요청을 파싱하기 위해 body-parser를 Express앱에 추가
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -23,9 +26,11 @@ app.use('/', userRoute);
 const mySerialPort = SerialPort.initializeSerialPort('/dev/ttyUSB0');
 
 var io = socket(server);
+// 클라이언트가 소켓에 연결되었을 때의 이벤트
 io.on("connection", function(socket) {
     console.log("User Connected: " + socket.id);
 
+    // 로봇 제어 명령을 수신하는 이벤트 헨들러
     socket.on("control", function(commandData) {
         console.log("Received control command:", commandData);
 
@@ -33,26 +38,36 @@ io.on("connection", function(socket) {
         let angularSpeed = 0;
 
         if (commandData.command === "forward") {
-            linearSpeed = 5.0;
+            linearSpeed = 4.0;
+            angularSpeed = 0;
         }
         else if (commandData.command === "Backward") {
-            linearSpeed = -5.0;
+            linearSpeed = -4.0;
+            angularSpeed = 0;
         }
         else if (commandData.command === "Left") {
-            angularSpeed = -5.5;
+            linearSpeed = 0;
+            angularSpeed = -3.0;
         }
         else if (commandData.command === "Right") {
-            angularSpeed = 5.5;
+            linearSpeed = 0;
+            angularSpeed = 3.0;
+        }
+        else if (commandData.command === "Stop") {
+            linearSpeed = 0.0;
+            angularSpeed = 0.0;
         }
         robotSpeedToRPMSpeed(linearSpeed, angularSpeed);
 
         putPNTVelCmd(mySerialPort, goal_rpm_speed[0], goal_rpm_speed[1]);
     });
 
+    // 채팅방에 입장하는 이벤트 헨들러
     socket.on("join", function(roomName) {
         var rooms = io.sockets.adapter.rooms;
         var room = rooms.get(roomName);
 
+        // 채팅방에 처음 입장할 경우, 새로운 방을 생성하거나 기존 방에 참여
         if (room == undefined) {
             socket.join(roomName);
             socket.emit("created");
@@ -67,36 +82,41 @@ io.on("connection", function(socket) {
         console.log(room);
     });
 
+    // 상대방이 준비되었음을 알리는 이벤트 헨들러
     socket.on("ready", function(roomName) {
         console.log("Ready");
         socket.broadcast.to(roomName).emit("ready");
     });
 
+    // ICE Candidate 를 전달하는 이벤트 헨들러
     socket.on("candidate", function(candidate, roomName) {
         console.log("Candidate");
         console.log(candidate);
         socket.broadcast.to(roomName).emit("candidate", candidate);
     });
 
+    // Offer를 전달하는 이벤트 헨들러
     socket.on("offer", function(offer, roomName) {
         console.log("Offer");
         console.log(offer);
         socket.broadcast.to(roomName).emit("offer", offer);
     });
 
+    // 메시지를 전달하는 이벤트 헨들러
+    socket.on("message", function(data) {
+        socket.broadcast.to(data.roomName).emit("message", data);
+    });
+
+    // Answer를 전달하는 이벤트 헨들러
     socket.on("answer", function(answer, roomName) {
         console.log("Answer");
         socket.broadcast.to(roomName).emit("answer", answer);
     });
 
+    // 채팅방에서 퇴장하는 이벤트 헨들러
     socket.on("leave", function(roomName) {
         socket.leave(roomName);
         socket.broadcast.to(roomName).emit("leave");
-    });
-
-    socket.on("message", function(data) {
-        console.log("Received message:", data);
-        socket.broadcast.to(data.roomName).emit("message", data);
     });
 
 });
